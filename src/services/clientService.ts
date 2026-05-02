875099932fbae3d0f70e1f90e1481659d6fa1d7a
jsw <csw9201@naver.com>
2026-05-02 13:33:22 +0900

    refactor: 가독성을 위해 함수의 정렬 순서 변경 (팀, 선수)

--- src/services/clientService.ts
+++ src/services/clientService.ts
@@ -2,10 +2,10 @@
 import { firebaseApiInstance } from '../api/firebaseClient'
 import { FIREBASE_API_ENDPOINT } from '../constant'
 import { getFirebaseURLPath } from '../utils/path'
+import { DEFAULT_API_PARAMS } from 'shared/params'
 import type { IFirebaseTeamDetail } from '../types'
 import type { FirebaseReturnPath } from '../utils/path'
 import type { IFirebasePlayer } from 'shared/api.types'
-import { DEFAULT_API_PARAMS } from 'shared/params'
 
 // tanstack query와 함께 사용할 때, 데이터를 가져오는 fetch 함수에서 try-catch를 사용하면
 // tanstack query의 error 상태가 동작하지 않을 수 있습니다
@@ -17,6 +17,8 @@ const fetchFirebaseData = async <T>(path: FirebaseReturnPath): Promise<T> => {
   return response.data
 }
 
+// 팀 조회
+
 // 하나의 팀 조회
 export const fetchTeam = async (
   teamId: number,
@@ -25,6 +27,17 @@ export const fetchTeam = async (
 
   return await fetchFirebaseData<IFirebaseTeamDetail>(getFirebaseURLPath(url))
 }
+
+// 리그 내 모든 팀 id 조회
+export const fetchTeamIdsInLeague = async (
+  leagueId: number = DEFAULT_API_PARAMS.league,
+): Promise<number[]> => {
+  const url = FIREBASE_API_ENDPOINT.LEAGUE_TEAM_IDS(leagueId)
+
+  return await fetchFirebaseData<number[]>(getFirebaseURLPath(url))
+}
+
+// 선수 조회
 
 // 선수 정보 조회
 export const fetchPlayer = async (
@@ -38,15 +51,6 @@ export const fetchPlayer = async (
 // 팀의 모든 선수 id 조회
 export const fetchTeamPlayerIds = async (teamId: number): Promise<number[]> => {
   const url = FIREBASE_API_ENDPOINT.TEAM_PLAYER_IDS(teamId)
-
-  return await fetchFirebaseData<number[]>(getFirebaseURLPath(url))
-}
-
-// 리그 내 모든 팀 id 조회
-export const fetchTeamIdsInLeague = async (
-  leagueId: number = DEFAULT_API_PARAMS.league,
-): Promise<number[]> => {
-  const url = FIREBASE_API_ENDPOINT.LEAGUE_TEAM_IDS(leagueId)
 
   return await fetchFirebaseData<number[]>(getFirebaseURLPath(url))
 }
