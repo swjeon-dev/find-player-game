4d3ae8fb74f10ec721ebcee07cb1cfe127ba64f9
jsw <csw9201@naver.com>
2026-05-07 10:51:06 +0900

    refactor: 선수 데이터 프리페칭 제거

--- src/components/layout/RootLayout.tsx
+++ src/components/layout/RootLayout.tsx
@@ -1,15 +1,9 @@
-import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
 import styled from 'styled-components'
 
 import { GlobalStyle } from '@/styles/GlobalStyle'
-import { queryClient } from '@/queryClient'
-import { fetchPlayersDataInLeague } from '@/services/clientService'
-import { REACT_QUERY_OPTIONS } from '@/api'
 import Header from '../Header'
-import { useRecoilValue } from 'recoil'
-import { leagueInfoState } from '@/atoms/quizState'
 
 const Container = styled.div`
   display: flex;
@@ -24,21 +18,6 @@ const Container = styled.div`
 `
 
 function RootLayout() {
-  const leagueInfo = useRecoilValue(leagueInfoState)
-
-  useEffect(() => {
-    queryClient.prefetchQuery({
-      queryKey: [leagueInfo.id, 'league', 'players'],
-      queryFn: () => fetchPlayersDataInLeague(leagueInfo.id),
-      ...REACT_QUERY_OPTIONS,
-    })
-    // queryClient.prefetchQuery({
-    //   queryKey: [DEFAULT_API_PARAMS.league, 'league', 'players'],
-    //   queryFn: () => fetchPlayersDataInLeague(DEFAULT_API_PARAMS.league),
-    //   ...REACT_QUERY_OPTIONS,
-    // })
-  }, [leagueInfo.id, leagueInfo.season])
-
   return (
     <>
       <Helmet>
