2273293ef477511066aeb36c1ac7eb1a716fb841
jsw <csw9201@naver.com>
2026-05-26 14:01:25 +0900

    fix(router): 라우터의 basename을 빌드/배포 환경에 따라 변경

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -46,6 +46,8 @@ const routes = [
   },
 ]
 
-const router = createBrowserRouter(routes, { basename: '/find-player-game' })
+const router = createBrowserRouter(routes, {
+  basename: import.meta.env.DEV ? '/' : '/find-player-game',
+})
 
 export default router
